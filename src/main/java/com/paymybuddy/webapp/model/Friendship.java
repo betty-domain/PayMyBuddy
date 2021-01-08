b0a25ce2e093b39e793ab91d19c3c2ab33d1f16f
betty Domain <betty.domain@free.fr>
2021-01-08 10:29:43 +0100

    Add IdClass on Friendship

--- src/main/java/com/paymybuddy/webapp/model/Friendship.java
+++ src/main/java/com/paymybuddy/webapp/model/Friendship.java
@@ -6,12 +6,13 @@ import lombok.Data;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.IdClass;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Data
-@Entity
+@Entity @IdClass(FriendshipId.class)
 @Table(name="user_friends")
 public class Friendship {
 
