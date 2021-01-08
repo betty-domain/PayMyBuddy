f282348374394697c723a64797a82e649098183f
betty Domain <betty.domain@free.fr>
2021-01-08 14:18:03 +0100

    implements Sprinc Security based on UserTable of PayMyBuddy WebApp

--- src/main/java/com/paymybuddy/webapp/service/SecurityConfig.java
+++ src/main/java/com/paymybuddy/webapp/service/SecurityConfig.java
@@ -1,14 +1,34 @@
 package com.paymybuddy.webapp.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public AuthenticationProvider authProvider()
+    {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(new BCryptPasswordEncoder());
+        return provider;
+    }
+
 
     @Override
     protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
@@ -17,10 +37,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
         http.authorizeRequests()
                 .antMatchers("/*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin();
+
+        http.httpBasic();
     }
 }
