ceecf53fc27f190b1e2c0f1743e2fde45bfb2824
betty Domain <betty.domain@free.fr>
2021-01-11 11:47:07 +0100

    add throws exception on user service interface

--- src/main/java/com/paymybuddy/webapp/service/IUserService.java
+++ src/main/java/com/paymybuddy/webapp/service/IUserService.java
@@ -1,14 +1,15 @@
 package com.paymybuddy.webapp.service;
 
 import com.paymybuddy.webapp.dto.UserDto;
+import com.paymybuddy.webapp.model.FunctionalException;
 import com.paymybuddy.webapp.model.User;
 
 import java.util.List;
 
 public interface IUserService {
-    User addUser(UserDto userDto);
+    User addUser(UserDto userDto) throws FunctionalException;
 
-    User updateUser(UserDto userDto);
+    User updateUser(UserDto userDto) throws FunctionalException;
 
     List<User> getAllUsers();
 
